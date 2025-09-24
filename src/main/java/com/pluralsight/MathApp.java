94894477459b95febabd15bf3dc1876e9eab7db8
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-24 15:06:44 -0400

    initial commit after max salary amount

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+int bobSalary = 45000;
+int garySalary = 80000;
+        System.out.println(Math.max(45000,80000));
+    }
+}
